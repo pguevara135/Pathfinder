013b06bee908c05ce2145ed7b9db3cd438dfed7d
pguevara135 <pguevara135@hotmail.com>
2020-06-14 17:59:55 -0300

    JS; Jqyery

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function Calcular() {
-    var valor1 = document.getElementById('valor-1').value;
-    var valor2 = document.getElementById('valor-2').value;
-
-    var resultado = parseInt(valor1) + parseInt(valor2);
-
-    document.getElementById('resultado').innerText = resultado;
-}
